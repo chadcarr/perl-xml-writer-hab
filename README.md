6973ba2386c77419dc7880613c6c947c42465be6
Chad Carr <ccarr@franzdoodle.com>
2019-03-17 20:34:05 -0400

    Checkpoint: working build

--- README.md
+++ README.md
@@ -1,5 +1,36 @@
-# xml-writer-hab
-Habitat package for perl XML::Writer module
+# Habitat package - perl-xml-writer
+Habitat package for perl module XML::Writer
 
 ## Usage
-This package may be used as a dependency for other Habitat packages.
+```
+cd <proj>
+hab studio enter
+build
+exit
+hab pkg upload results/<pkg>.hart
+```
+
+## Setting up "hab studio enter"
+* Establish up your origin on builder
+    * Generate origin key pair
+    * Generate personal access token
+* "hab cli setup"
+    * Enter personal access token
+    * Enter control secret (see also "Windows tricks")
+* Start habitat superviser "hab sup run" (see also "Windows tricks")
+* Start habitat studio in your project directory "hab studio enter"
+
+## Windows tricks
+Problem: "hab sup run" doesn't work from Ubuntu on Windows and "hab studio enter" on Windows starts a powershell session by default, even from git bash. So do "hab sup run" directly on Windows (git bash, powershell, or cmd.exe), then do "hab studio enter" from Ubuntu.
+
+Supervisor control secrets must match: copy from Windows file
+C:\hab\sup\default\CTL_SECRET into Ubuntu ~/.hab/etc/cli.toml:
+
+```
+ctl_secret = "fe+j4Ga+44AvwI6BcjlF1m3V+Y+0+AMNrZIWwIXJlqL2LaPny1qmYTlmJHbXeN1+7SCFRadp3wTcembSiVolGw=="
+```
+
+Or rerun "hab cli setup"
+
+## Maintainer
+Chad Carr <ccarr@franzdoodle.com>
--- plan.sh
+++ plan.sh
@@ -1,10 +1,25 @@
-pkg_name=xml-writer
-pkg_version=0.625
+pkg_name=perl-xml-writer
 pkg_origin=chadcarr
-pkg_description="Habitat package for perl XML::Writer module"
-pkg_source=https://cpan.metacpan.org/authors/id/J/JO/JOSEPHW/XML-Writer-${pkg_version}.tar.gz
+pkg_maintainer="Chad Carr <ccarr@franzdoodle.com>"
+pkg_version=0.625
+pkg_license=(unrestricted)
+pkg_description="Perl XML::Writer module"
+pkg_upstream_url=https://metacpan.org/release/XML-Writer
+pkg_source=https://cpan.metacpan.org/authors/id/J/JO/JOSEPHW/XML-Writer-$pkg_version.tar.gz
+pkg_shasum=e080522c6ce050397af482665f3965a93c5d16f5e81d93f6e2fe98084ed15fbe
 pkg_deps=(core/perl)
 pkg_build_deps=(core/make)
+
+# Unpack to $pkg_name-$pkg_version; default habitat behavior unpacks tarball
+# unmodified (XML-Writer-0.625), but subsequent callbacks start by default in
+# $pkg_name-$pkg_version, so we unpack there to avoid later overrides.
+do_unpack() {
+    local source_file=$HAB_CACHE_SRC_PATH/$pkg_filename
+    local unpack_dir=$HAB_CACHE_SRC_PATH/$pkg_name-$pkg_version
+    build_line "Unpacking $pkg_filename to custom cache dir: $unpack_dir"
+    mkdir -p $unpack_dir
+    tar xf $source_file --strip 1 --directory $unpack_dir
+}
 
 do_build() {
     perl Makefile.PL PREFIX=$pkg_prefix
@@ -13,8 +28,4 @@ do_build() {
 
 do_check() {
     make test
-}
-
-do_install() {
-    make install
 }
